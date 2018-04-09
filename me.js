6447943c7e19e0c830881b7fac0fddcc9012e4f8
Dot <dotcra@gmail.com>
2018-04-09 12:19:32 +0800

    nth

---
+++ me.js
@@ -0,0 +1,46 @@
+var me = {
+	'en': {
+		'title': 'resume',
+		'name': 'dot craft',
+		'role': 'full-stack developer, linux administrator',
+		'about_t': 'about',
+		'skills_t': 'skills',
+		'sn_t': 'social network',
+		'exp_t': 'experiences',
+		'edu_t': 'education',
+		'intro': 'i am hahaha',
+		'devenv_t': 'development environment',
+		'devenv': 'vim + bash, Fedora 27',
+		'proglang_t': 'language',
+		'proglang': 'javascript, php, python, bash',
+		'location': 'would like: remote, startups, shanghai',
+		'site_t': 'website',
+		'site': 'https://dotcra.com',
+		'mail_t': 'email',
+		'mail': 'job@dotcra.com',
+		'skills': [
+			'skill 1',
+			'skill 2',
+			'skill 3',
+			'skill 4',
+			'skill 5',
+			'skill 6',
+			'skill 7', 
+			'skill 8', 
+			'skill 9'
+		],
+		'exps': [
+			{
+				'com': 'google ceo',
+				'job': 'i am goddamn ceo'
+			},
+			{
+				'com': 'facebook ceo',
+				'job': 'i am goddamn ceo. Again'
+			}
+		],
+		'edu': 'self-taught'
+	},
+	'zh': {
+	}
+};
