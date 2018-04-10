55c852a5ccd8b34f7e1016a597a624c8f17bfffc
Dot <dotcra@gmail.com>
2018-04-10 16:37:11 +0800

    nth

--- js/resume.js
+++ js/resume.js
@@ -98,8 +98,8 @@
 		}
 	};
 
-	//var lang = 'zh';
 	var lang = (navigator.language || navigator.userLanguage).substr(0, 2);
+	if (lang !== 'zh') lang = 'en';
 	var en = document.getElementById('en');
 	var zh = document.getElementById('zh');
 	en.addEventListener('click', ch);
