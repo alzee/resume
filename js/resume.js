992d543b82cb3c62bb2d862837ef36c3119bacbd
Arc Tod <ctod@msn.com>
2018-10-10 17:12:16 +0800

    add resume.note

--- js/resume.js
+++ js/resume.js
@@ -20,6 +20,7 @@
 			sn_t: '社交网络',
 			exp_t: '工作经历',
 			edu_t: '教育经历',
+			note: '该简历有两个 git 分支，您正在查看 WTF 分支',
 			intro: '懂点 Linux ，比如 Fedora, Debian, RedHat/CentOS, LFS 等发行版。目前兴趣在 web 开发，主技术栈为 php, symfony 4, twig, bootstrap 4, vue.js, mustache.js 及原生 javascript ，后端方向，可做全栈，可独立开发。具备团队组织协调能力。简练、果敢、沉着、细致、好学、求真。',
 			devenv_t: '开发环境: ',
 			devenv: 'bash, screen, vim, Fedora',
