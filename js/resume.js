1e62539e26b74476574cf96e17031f51f7df396a
Arc Zee <z@arcz.ee>
2019-02-21 12:49:57 +0800

    nth

--- js/resume.js
+++ js/resume.js
@@ -21,7 +21,7 @@
 			sn_t: '社交网络',
 			exp_t: '工作经历',
 			edu_t: '教育经历',
-			intro: '熟悉企业内软硬件及网络运维。擅长 Linux 系统工程，熟悉 Fedora, Debian, RedHat/CentOS, LFS 等。有 web 开发经验，主技术栈为 php, symfony 4, twig, bootstrap 4, vue.js, mustache.js 及原生 javascript ，后端方向，可做全栈，可独立开发。具备团队组织协调能力。专业、高效、简练、果敢、沉着、细致、好学、求真。',
+			intro: '熟悉企业软硬件及网络运维。擅长 Linux 系统工程，熟悉 Fedora, Debian, RedHat/CentOS, LFS 等。有 web 开发经验，主技术栈为 php, symfony 4, twig, bootstrap 4, vue.js, mustache.js 及原生 javascript ，后端方向，可做全栈，可独立开发。具备团队组织协调能力。专业、高效、简练、果敢、沉着、细致、好学、求真。',
 			devenv_t: '开发环境: ',
 			devenv: 'bash, screen, vim, Fedora',
 			proglang_t: '语言： ',
